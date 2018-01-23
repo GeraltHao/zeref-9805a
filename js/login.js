161f926a386b29b7e5638ad3f1c2fe8a2dad7f2d
GeraltHao <assasin.vic@mail.ru>
2018-01-23 14:19:37 +0300

    Добавлен секретный переход на сайт по паролю, и добавлен преоадер. возвращена страница 404. Изменена картинка "обо мне" на анимацию.

---
+++ js/login.js
@@ -0,0 +1,19 @@
+function login() {
+    var pass = window.prompt('Введите пароль: ', '');
+    if (pass==null || pass==''){
+        console.log('Доступ запрещен')
+}
+    else{
+        window.open(pass+'.html');
+    }
+}
+
+function login() {
+    var pass = window.prompt('Введите пароль: ', '');
+    if (pass==null || pass==''){
+        console.log('Доступ запрещен')
+}
+    else{
+        window.open(pass+'.html');
+    }
+}
